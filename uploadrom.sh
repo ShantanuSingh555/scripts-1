9bc4add48c9422cd7832cb200634f6ec8a288f1a
merothh <merothh@gmail.com>
2018-05-16 22:17:16 +0000

    uploadrom.sh: get which device we are dealing with

--- uploadrom.sh
+++ uploadrom.sh
@@ -1,16 +1,16 @@
 #!/bin/bash  
 
-#get the latest zip i built
-file=$(ls out/target/product/mido/*201*.zip | tail -n 1)
+#get the latest zip built
+file=$(ls out/target/product/$1/*201*.zip | tail -n 1)
 
 #send a direct link to my testing group
-if [ $1 = "gdrive" ]; then
+if [ $2 = "gdrive" ]; then
 	id=$(gdrive upload --parent 13eTar2JcxqYJz93TLL86DucTw6iG4ZjR $file | grep "Uploaded" | cut -d " " -f 2) 
 	telegram-send "New Build is up:- https://drive.google.com/uc?export=download&id=$id"  
 fi
 
 #upload to afh through ftp
-if [ $1 = "afh" ]; then
+if [ $2 = "afh" ]; then
 	echo "user: "
 	read user
 	echo "password: "
