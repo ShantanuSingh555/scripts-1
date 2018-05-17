9b07505ed423bd219d9c34fb6308a88fdd23b2db
merothh <merothh@gmail.com>
2018-05-17 20:40:41 +0000

    buildrom.sh: cleanup
      * should have used an elif statement to check for afh before
      * incase $2 is blank, prevent the test from being errored out
      * fixed up the tab spacing as well

--- buildrom.sh
+++ buildrom.sh
@@ -19,18 +19,17 @@ if [ $(grep -c "### make failed" build.log) = 0 ]; then
 	file=$(ls out/target/product/$1/*201*.zip | tail -n 1)
 
 	#if gdrive send a direct link to my testing group
-	if [ $2 = "gdrive" ]; then
+	if [ "$2" = "gdrive" ]; then
 		id=$(gdrive upload --parent 13eTar2JcxqYJz93TLL86DucTw6iG4ZjR $file | grep "Uploaded" | cut -d " " -f 2)
 		telegram-send "New Build is up:- https://drive.google.com/uc?export=download&id=$id"
-	fi
 
 	#if afh upload to afh through ftp
-	if [ $2 = "afh" ]; then
-	echo "user: "
-	read user
-	echo "password: "
-	read password
-	curl -T  $file ftp://uploads.androidfilehost.com --user $user:$password
+	elif [ "$2" = "afh" ]; then
+		echo "user: "
+		read user
+		echo "password: "
+		read password
+		curl -T  $file ftp://uploads.androidfilehost.com --user $user:$password
 	fi
 
 else
