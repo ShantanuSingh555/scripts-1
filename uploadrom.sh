75a46bd673c240847124d30a0ba3cfcec44b99e0
merothh <merothh@gmail.com>
2018-05-17 01:30:27 +0000

    uploadrom.sh: overhaul to build the rom as well
      * so that we can check if the build has failed and send messages accordingly
        instead of giving people false hope :3
      * make life easier

--- uploadrom.sh
+++ uploadrom.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 #usage:
 #	Place in root directory of build folder and
-#        bash uploadrom.sh devicecodename gdrive/afh
+#        bash buildrom.sh devicecodename gdrive/afh
 
-#get the latest zip built
-file=$(ls out/target/product/$1/*201*.zip | tail -n 1)
+#prepare the environment
+rm -rf venv
+virtualenv2 venv
+source venv/bin/activate
+export LC_ALL=C
+source build/envsetup.sh
 
-#send a direct link to my testing group
-if [ $2 = "gdrive" ]; then
-	id=$(gdrive upload --parent 13eTar2JcxqYJz93TLL86DucTw6iG4ZjR $file | grep "Uploaded" | cut -d " " -f 2) 
-	telegram-send "New Build is up:- https://drive.google.com/uc?export=download&id=$id"  
-fi
+#build rom and log the build
+brunch $1 |& tee build.log
 
-#upload to afh through ftp
-if [ $2 = "afh" ]; then
+if [ $(grep -c "### make failed" build.log) = 0 ]; then
+
+	#get the latest zip built
+	file=$(ls out/target/product/$1/*201*.zip | tail -n 1)
+
+	#if gdrive send a direct link to my testing group
+	if [ $2 = "gdrive" ]; then
+		id=$(gdrive upload --parent 13eTar2JcxqYJz93TLL86DucTw6iG4ZjR $file | grep "Uploaded" | cut -d " " -f 2)
+		telegram-send "New Build is up:- https://drive.google.com/uc?export=download&id=$id"
+	fi
+
+	#if afh upload to afh through ftp
+	if [ $2 = "afh" ]; then
 	echo "user: "
 	read user
 	echo "password: "
 	read password
 	curl -T  $file ftp://uploads.androidfilehost.com --user $user:$password
+	fi
+
+else
+	#let the boys know it's time to sleep
+	telegram-send "#blamemerothh"
 fi
